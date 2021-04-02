b18fa67ff5d784c6bc89b7a8e0acca6fc8c5b569
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2021-04-02 20:02:26 +0200

    inventory

---
+++ InventorySystem/Assets/Scripts/Item.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public enum ItemType {HEALTH, DAMAGE};
+
+public class Item : MonoBehaviour {
+
+	public ItemType type;
+
+	public Sprite spriteNeutral;
+	public Sprite spriteHighlighted;
+
+	public int maxStackSize;
+
+
+	public void Use() {
+		switch (type) {
+		case ItemType.HEALTH:
+			Debug.Log ("Health potion used.");
+			break;
+		case ItemType.DAMAGE:
+			Debug.Log ("Damage potion drank.");
+			break;
+		}
+	}
+}
---
+++ InventorySystem/Assets/Scripts/Slot.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IPointerClickHandler
+{
+	
+	
+	private Stack<Item> items;
+
+	public Text stackTxt;
+	
+
+	public Sprite slotEmpty;
+
+	public Sprite slotHighlight;
+	
+	
+
+	public Stack<Item> Items
+	{
+		get { return items; }
+		set { items = value; }
+	}
+	
+
+	public bool IsEmpty
+	{
+		get { return items.Count == 0; }
+	}
+	
+
+	public bool IsAvailable
+	{
+		get {return CurrentItem.maxStackSize > items.Count; }
+	}
+	
+
+	public Item CurrentItem
+	{
+		get { return items.Peek(); }
+	}
+	
+	
+	void Start () 
+	{   
+		items = new Stack<Item>();
+		
+		RectTransform slotRect = GetComponent<RectTransform>();
+		
+		RectTransform txtRect = stackTxt.GetComponent<RectTransform>();
+		
+		int txtScleFactor = (int)(slotRect.sizeDelta.x * 0.60);
+		
+		stackTxt.resizeTextMaxSize = txtScleFactor;
+		stackTxt.resizeTextMinSize = txtScleFactor;
+		
+	
+		txtRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotRect.sizeDelta.x);
+		txtRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotRect.sizeDelta.y);
+	}
+	
+
+	public void AddItem(Item item)
+	{
+		items.Push(item);
+		
+		if (items.Count > 1) 
+		{
+			stackTxt.text = items.Count.ToString(); 
+		}
+		
+		ChangeSprite(item.spriteNeutral, item.spriteHighlighted); 
+	}
+
+	public void AddItems(Stack<Item> items)
+	{
+		this.items = new Stack<Item>(items);
+		
+		stackTxt.text = items.Count > 1 ? items.Count.ToString() : string.Empty; 
+		
+		ChangeSprite(CurrentItem.spriteNeutral, CurrentItem.spriteHighlighted); 
+	}
+	
+
+	private void ChangeSprite(Sprite neutral, Sprite highlight)
+	{
+		GetComponent<Image>().sprite = neutral;
+		
+		SpriteState st = new SpriteState();
+		st.highlightedSprite = highlight;
+		st.pressedSprite = neutral;
+		
+		GetComponent<Button>().spriteState = st;
+	}
+	
+
+	private void UseItem()
+	{
+		if (!IsEmpty) 
+		{
+			items.Pop().Use(); 
+			
+			stackTxt.text = items.Count > 1 ? items.Count.ToString() : string.Empty; 
+			
+			if (IsEmpty) 
+			{
+				ChangeSprite(slotEmpty, slotHighlight); 
+				
+				Inventory.EmptySlots++; 
+			}
+		}
+	}
+	
+	public void ClearSlot()
+	{   
+		items.Clear();
+		
+		ChangeSprite(slotEmpty, slotHighlight);
+		
+		stackTxt.text = string.Empty;
+	}
+	
+
+	public void OnPointerClick(PointerEventData eventData)
+	{   
+		if (eventData.button == PointerEventData.InputButton.Right) 
+		{
+			UseItem();
+		}
+	}
+}
